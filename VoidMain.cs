d57dd272fafc0b2dcbacd6e8ead1a36d1376cd75
Jeroen P. Broks <facebook@tbbs.nl>
2019-07-09 08:23:23 +0200

    Generated stuff

---
+++ VoidMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NALA
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class VoidMain
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new NALACore())
+                game.Run();
+        }
+    }
+}
